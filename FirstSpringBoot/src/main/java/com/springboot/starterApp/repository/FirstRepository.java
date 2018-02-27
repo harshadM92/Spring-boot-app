dc3e351d3623f48f2156bceb73a773b285f9d3e9
Harshad-Maniyar <maniyar.harshad92@gmail.com>
2018-02-27 23:45:39 +0530

    delted folders

--- FirstSpringBoot/src/main/java/com/springboot/starterApp/repository/FirstRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.starterApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.starterApp.DAO.FirstEntity;
-
-public interface FirstRepository extends JpaRepository<FirstEntity, String>{
-
-}
